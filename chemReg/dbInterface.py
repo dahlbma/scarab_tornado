7f2c807eff7e662e3ad138ee131afcac609df8df
Mats Dahlberg <dahlbma@gmail.com>
2021-10-20 11:42:56 +0200

    search->update

--- chemReg/dbInterface.py
+++ chemReg/dbInterface.py
@@ -1,18 +1,22 @@
 import requests
 import json
 
-def listify(data):
+def listify(data, addBlank=True):
     res = data.content.decode()
     res = json.loads(res)
-    cleanList = list(' ', )
+    cleanList = list()
+    if addBlank:
+        cleanList.append(' ')
     for i in res:
         cleanList.append(i[0])
     return cleanList
 
 def searchValue(target, value, token):
-    r = requests.put('http://esox3.scilifelab.se:8082/api/search',
+    r = requests.get('http://esox3.scilifelab.se:8082/api/search',
                      params={'column': target, 'value': value},
                      headers={'token': token})
+    cleanList = listify(r, False)
+    return cleanList
 
 def updateValue(target, value, token, regno):
     r = requests.put('http://esox3.scilifelab.se:8082/api/update',
@@ -35,10 +39,39 @@ def getSubmitters(token):
     cleanList = listify(r)
     return cleanList
 
-def getProjects(token):
+def getTextColumn(token, column, regno):
+    r = requests.get('http://esox3.scilifelab.se:8082/api/getTextColumn',
+                     params={'column': column, 'regno': regno},
+                     headers={'token': token})
+    cleanList = listify(r, False)
+    return cleanList[0]
+    
+def getColComboData(token, regno, column):
+    r = requests.get('http://esox3.scilifelab.se:8082/api/getColComboData',
+                     params={'column': column},
+                     headers={'token': token})
+    cleanList = listify(r)
+    r = requests.get('http://esox3.scilifelab.se:8082/api/getRegnoData',
+                     params={'regno': regno, 'column': column},
+                     headers={'token': token})
+    currentValue = listify(r, False)[0]
+    if currentValue != None:
+        cleanList.remove(currentValue)
+        cleanList.insert(0, currentValue)
+    return cleanList
+
+
+def getProjects(token, regno):
     r = requests.get('http://esox3.scilifelab.se:8082/api/getProjects',
                      headers={'token': token})
     cleanList = listify(r)
+    r = requests.get('http://esox3.scilifelab.se:8082/api/getRegnoData',
+                     params={'regno': regno, 'column': 'project'},
+                     headers={'token': token})
+    currentValue = listify(r, False)[0]
+    if currentValue != None:
+        cleanList.remove(currentValue)
+        cleanList.insert(0, currentValue)
     return cleanList
 
 def getCompoundTypes(token):
