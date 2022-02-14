f7ffc1a48537bfd836f474717f32f107b9c8865c
ChrillePille <christian@weigelt.se>
2022-02-14 17:55:19 +0100

    fix path

--- chemReg/main.py
+++ chemReg/main.py
@@ -58,7 +58,7 @@ try:
     widget.resize(windowWidth, windowHeight)
 
     widget.show()
-    app.setWindowIcon(QtGui.QIcon('asssets/chem.ico'))
+    app.setWindowIcon(QtGui.QIcon('assets/chem.ico'))
     widget.setWindowIcon(QtGui.QIcon('assets/chem.ico'))
     sys.exit(app.exec_())
 except Exception as e:
