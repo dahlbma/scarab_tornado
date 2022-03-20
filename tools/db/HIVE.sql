d01c9f6a03441dc5d0f8bea8b451d68118b6afe5
Mats Dahlberg <dahlbma@gmail.com>
2022-03-20 19:45:52 +0100

    Update HIVE.sql

--- tools/db/HIVE.sql
+++ tools/db/HIVE.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
 --
--- Host: localhost    Database: HIVE
+-- Host: localhost    Database: hive
 -- ------------------------------------------------------
 -- Server version	8.0.26
 
@@ -14,3 +14,64 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `project_details`
+--
+
+DROP TABLE IF EXISTS `project_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `project_details` (
+  `pkey` bigint NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(40) DEFAULT NULL,
+  `DESCRIPTION` text,
+  `HTTP` text,
+  `COMMENTS` text,
+  `CREATED_DATE` datetime DEFAULT NULL,
+  `CREATED_BY_PKEY` bigint DEFAULT NULL,
+  `UPDATED_DATE` datetime DEFAULT NULL,
+  `UPDATED_BY_PKEY` double DEFAULT NULL,
+  `TERMINATED_DATE` text,
+  `project_leader` varchar(10) DEFAULT NULL,
+  `REQUIRED_ROLE` text,
+  PRIMARY KEY (`pkey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_details`
+--
+
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_details` (
+  `pkey` bigint NOT NULL AUTO_INCREMENT,
+  `userid` varchar(40) DEFAULT NULL,
+  `firstname` varchar(40) DEFAULT NULL,
+  `lastname` varchar(40) DEFAULT NULL,
+  `HIVELOCATION` text,
+  `HIVEPARENTLOCATION` text,
+  `email` varchar(80) DEFAULT NULL,
+  `TERMINATED_DATE` datetime DEFAULT NULL,
+  `unix_account` varchar(4) DEFAULT NULL,
+  `location` varchar(30) DEFAULT NULL,
+  `organisation` varchar(50) DEFAULT NULL,
+  `fullname` varchar(82) DEFAULT NULL,
+  `CREATED_DATE` datetime DEFAULT NULL,
+  `organization` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`pkey`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-20 13:19:36
