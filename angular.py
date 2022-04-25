15a3442ce6124dc456aaf35e487d69b1e59c5e9e
ChrillePille <christian@weigelt.se>
2022-04-25 11:28:37 +0200

    fix static file handler

--- angular.py
+++ angular.py
@@ -186,9 +186,7 @@ def make_app():
         (r"/getChemRegBinary/(?P<os_name>[^\/]+)", dbInterface.GetChemRegBinary), # upload
         (r"/uploadVersionNo", dbInterface.UploadVersionNo),
         (r"/uploadLauncher", dbInterface.UploadLauncher), # upload
-        (r"/getChemRegLauncher/Windows", web.StaticFileHandler, {"path": "dist/Windows/chemreg.exe"}),
-        (r"/getChemRegLauncher/Linux", web.StaticFileHandler, {"path": "dist/Linux/chemreg"}),
-        (r"/getChemRegLauncher/Darwin", web.StaticFileHandler, {"path": "dist/Darwin/chemreg"})
+        (r"/getChemRegLauncher/(.*)", web.StaticFileHandler, {"path": "dist/"})
     ], **settings)
 
 if __name__ == "__main__":
