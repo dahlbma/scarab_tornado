20aa1cb2e0ffcb1d075dc20dbf902d45e7179397
Mats Dahlberg <dahlbma@gmail.com>
2022-05-29 13:21:14 +0200

    Prepare the move from BeeHive to Scarab

---
+++ tools/getCDDdata.sh
@@ -0,0 +1,9 @@
+source /home/mats.dahlberg/.bashrc
+echo
+echo "Starting time:"
+date
+python /home/mats.dahlberg/venv/cdd_sync/cddToLocal.py
+echo
+echo "Finished time:"
+date
+echo "#####################################################################"
